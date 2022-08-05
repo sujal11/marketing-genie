8fb2133ae40b0ba8bd8c0cf3a71cc0b56e0e87e8
sona444 <sonakshi2500@gmail.com>
2022-08-05 14:50:21 +0530

    sentiment analysis done

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# uhackathon3.0
+# marketing-reviewer
+ 
--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from calendar import month
 from flask import Flask, render_template, request
+import pandas as pd
+import nltk
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from dotenv import load_dotenv
+from openpyxl import load_workbook
+import sqlite3
+import utils
+from nltk.tokenize import word_tokenize
+import nltk
+import locationtagger
+from statistics import mean
+import geopy
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
   
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+load_dotenv()
+
 app = Flask(__name__)
 
 filename=''
@@ -13,7 +31,6 @@ def index():
 
 @app.route('/upload-dataset', methods=['GET','POST'])
 def main():
-    
     f = request.files['file'] #File input
     structured=request.form.get('structure')
     if not f:
@@ -143,5 +160,50 @@ def main():
         return render_template('check.html', products=list(products), final_report=data_report, db=filename_for_database, structured=True)
     else:
         return render_template('check.html', products=list(products), final_report=data_report, db=filename_for_database)
+
+@app.route('/get-insights', methods=['GET','POST'])
+def insights():
+    #called for insights of products
+    db=request.form.get("db")
+    global filename_for_database
+    product=str(request.form.get('products'))
+    products=(product,)
+    Reviews=request.form.get('review')
+    order_status=request.form.get('order_status')
+    #For unstructured dataset Reviews are listed based on the result of sentiment analysis
+    if Reviews=='True':
+        filename_for_database=db
+        con=sqlite3.connect(filename_for_database) #connecting to the database
+        cursor=con.cursor()
+        cursor.execute('SELECT review_text FROM Dataset WHERE product_name = ?',products)
+        data=cursor.fetchall()
+        nltk.download('vader_lexicon')
+        reviews={'neg':0,'pos':0}
+        for review in data:
+            sid=SentimentIntensityAnalyzer()
+            sa=sid.polarity_scores(str(review))
+            if sa['neg']>sa['pos']:
+                reviews['neg']=reviews['neg']+1
+            else:
+                reviews['pos']=reviews['pos']+1
+        return { 'review':reviews }
+
+    #For structured dataset OrderStatuses are listed:
+    if order_status=='True':
+        filename_for_database=db
+        con=sqlite3.connect(filename_for_database) #connecting to the database
+        cursor=con.cursor()
+        cursor.execute('SELECT product_name, product_image, order_status FROM Dataset WHERE product_name = ?',products)
+        data=cursor.fetchall()
+        img=data[0][1]
+        status={}
+        for i in data:
+            if i[2] in status.keys():
+                status[i[2]]=status[i[2]]+1
+            else:
+                status[i[2]]=1
+    return {'img':img, 'status':status}
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
